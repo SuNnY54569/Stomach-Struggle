766e54f0c71e0a0a519188d16a97afb0c9d4595a
nattapatq <nattapat.sure@bumail.net>
2024-10-11 17:49:00 +0700

    cutsceneUpdate
    ได้cutseneแล้วแต่ยังไม่ได้dialogue

---
+++ Assets/Script/cutScene/writerEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class writerEffect : MonoBehaviour
+{
+    [Header("UI Components")]
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Typewriter Settings")]
+    [SerializeField] private float typingSpeed = 0.05f;
+
+    [SerializeField]private string characterName;
+    [SerializeField]private string dialogueContent;
+
+    private void Start()
+    {
+        StartCoroutine(ShowDialogue(characterName, dialogueContent));
+    }
+
+    private IEnumerator ShowDialogue(string name, string content)
+    {
+        dialogueText.text = name;
+
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (char letter in content.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
