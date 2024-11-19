de785cafe98281579d140f34f4d0252d4052f25c
nattapatq <nattapat.sure@bumail.net>
2024-11-19 20:48:27 +0700

    update skip sound

--- Assets/Script/cutScene/nextScene.cs
+++ Assets/Script/cutScene/nextScene.cs
@@ -7,6 +7,7 @@ public class nextScene : MonoBehaviour
 {
     public void LoadNextScene()
     {
+        SoundManager.PlaySound(SoundType.UIClick, VolumeType.SFX);
         SceneManagerClass.Instance.LoadNextScene();
     }
 }
