ee8553d034e8c02548952a63879d18fe08d14804
Sorawis Leepiyasakulchai <sorawisson@gmail.com>
2024-12-09 21:08:18 +0700

    Update waterShop.cs

--- Assets/Script/ChooseWater/waterShop.cs
+++ Assets/Script/ChooseWater/waterShop.cs
@@ -11,32 +11,78 @@ public class waterShop : MonoBehaviour
     [SerializeField] private GameObject[] objectsToClose;
     [SerializeField] private GameObject[] objectsToOpen;
     [SerializeField] private bool isReturnButton;
+    
+    private bool isPointerOver = false;
 
     private void Start()
     {
         GameManager.Instance.SetScoreTextActive(false);
     }
-
-    private void OnMouseOver()
+    
+    private void Update()
     {
         if (GameManager.Instance.isGamePaused) return;
-        sprite.color = Color.gray;
+
+        // Detect touch or mouse click
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            HandlePointerDown();
+        }
+
+        // Check hover-like behavior
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            HandleMouseHover();
+        }
     }
 
-    private void OnMouseExit()
+    private void HandlePointerDown()
     {
-        if (GameManager.Instance.isGamePaused) return;
-        sprite.color = Color.white;
+        // Check if the pointer is over this object
+        Vector2 pointerPosition = GetPointerPosition();
+        RaycastHit2D hit = Physics2D.Raycast(pointerPosition, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            ToggleObjects();
+            SoundManager.PlaySound(SoundType.UIClick, VolumeType.SFX);
+            sprite.color = Color.white;
+        }
     }
 
-    private void OnMouseDown()
+    private void HandleMouseHover()
     {
-        if (GameManager.Instance.isGamePaused) return;
-        
-        ToggleObjects();
+        Vector2 pointerPosition = GetPointerPosition();
+        RaycastHit2D hit = Physics2D.Raycast(pointerPosition, Vector2.zero);
 
-        SoundManager.PlaySound(SoundType.UIClick,VolumeType.SFX);
-        sprite.color = Color.white;
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            if (!isPointerOver)
+            {
+                sprite.color = Color.gray;
+                isPointerOver = true;
+            }
+        }
+        else
+        {
+            if (isPointerOver)
+            {
+                sprite.color = Color.white;
+                isPointerOver = false;
+            }
+        }
+    }
+    
+    private Vector2 GetPointerPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+        else
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
     }
     
     private void ToggleObjects()
